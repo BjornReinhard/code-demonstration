281458e265fce21c30837b632c8d732ac1762913
RainhardBjorn <sda.isu@gmail.com>
2020-10-16 01:46:52 -0500

    dev::init frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div className={styles.App}>
+        <div className={styles.hero}>
+            Code demonstration project
+        </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "fontsource-source-sans-pro/300-normal.css";
+import './index.module.css';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
