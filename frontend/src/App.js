e914e472c5b07cd61eb82f73d42485d7b8bcb0b4
Daniil Stepanov <Daniil.Stepanov@excelian.com>
2020-10-16 18:01:40 -0500

    dev::add react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,43 @@
 import React from 'react';
 import styles from './App.module.css';
 
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import Projects from "./layout/Projects/Projects";
+
 function App() {
-  return (
-    <div className={styles.App}>
-        <div className={styles.hero}>
+    return (
+        <div className={styles.main} role="main">
             Code demonstration project
+
+            <Router>
+                <div>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/projects">Projects</Link>
+                        </li>
+                    </ul>
+
+                    <hr />
+                    <Switch>
+                        <Route exact path="/">
+                            <div>Nothing</div>
+                        </Route>
+                        <Route path="/projects">
+                            <Projects />
+                        </Route>
+                    </Switch>
+                </div>
+            </Router>
         </div>
-    </div>
-  );
+    );
 }
 
 export default App;
